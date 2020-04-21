6e66b3a6380252e391ad14f13d568a86c0f61ac1
Asten Fuller <astenhfuller@gmail.com>
2020-04-20 17:40:43 -0700

    images are dynamically rendered on home page

--- client/src/js/home/homePage.jsx
+++ client/src/js/home/homePage.jsx
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import Header from '../header/Header';
-import sudoku from '../../assets/sudoku.png'
 import { Link } from 'react-router-dom';
+import images from '../../assets/games/*.png';
 
 export default class HomePage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			games: [],
+		}
+	}
+
+	componentDidMount() {
+		fetch('/api/Games')
+			.then(res => res.json())
+			.then(allGames => this.setState({ games: allGames }));
+	}
+
 	render() {
+		let keyCounter = 0;
+
 		return (
 			<div className='home-container'>
 				<Header />
 				<div className='game-container'>
-					<div className='each-game-box'>
-						<h3 className='each-game-title'>Sudoku</h3>
-						<Link to={'/sudoku'}>
-							<img className='each-game' src={sudoku}/>
-						</Link>
-					</div>
+					{this.state.games.map(game => {
+						keyCounter++;
+
+						return (
+							<div className='each-game-box' key={keyCounter}>
+								<h3 className='each-game-title'>{game.title}</h3>
+								<Link to={`/${game.title}`}>
+									<img className='each-game' src={Object.values(images).filter(value => value.includes(game.image.toLowerCase()))[0]} />
+								</Link>
+							</div>
+						)
+					})}
 				</div>
 			</div>
 		)
